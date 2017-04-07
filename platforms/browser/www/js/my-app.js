9820c6c76079909cdb542cecae740f330fa3d8d8
Jhon Moreira <jonh.m.10@gmail.com>
2017-04-06 19:43:20 -0600

    Logica de registro completa

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -1,23 +1,77 @@
-// Initialize app
-var myApp = new Framework7();
+// Inicializa la aplicacion
+var myApp = new Framework7({
+            material : true, //Activa el tema de material css en framework7
+        });
 
-
-// If we need to use custom DOM library, let's save it to $$ variable:
+// Si necesito usar Libreria personalizada del DOM se salva en $$
 var $$ = Dom7;
 
-// Add view
+// Añade una vista
 var mainView = myApp.addView('.view-main', {
-    // Because we want to use dynamic navbar, we need to enable it for this view:
-    dynamicNavbar: true
+    dynamicNavbar: true //Inicializa la vista con navbar dinamica
 });
 
-// Handle Cordova Device Ready Event
+// Maneja el evento de cordova de dispositivo listo
 $$(document).on('deviceready', function() {
-    console.log("Device is ready!");
+    console.log("El dispositivo está listo");
 });
 
 
-//Login screen loaded
+//Funcion anonima al cargarse la pantalla de registro
 myApp.onPageInit('register', function (page) {
-    //myApp.alert('Se cargo la pagina');
-})
+
+    // Metodo submit del formulario 
+    $( "#registrarClienteForm" ).submit(function( event ) {
+      
+      // Previene el submit normal y salto de pantalla
+      event.preventDefault();
+      
+      // Obtiene los datos del formulario
+      var $form = $( this ),
+        name = $form.find( "input[name='name']" ).val(),
+        email = $form.find( "input[name='email']" ).val(),
+        password = $form.find( "input[name='password']" ).val(),
+        cedula = $form.find( "input[name='cedula']" ).val(),
+        telefono = $form.find( "input[name='telefono']" ).val(),
+        fecha_nacimiento = $form.find( "input[name='fecha_nacimiento']" ).val(),
+        domicilio = $form.find( "input[name='domicilio']" ).val(),
+        url = $form.attr( "action" );
+
+      //Muestra el preloader al inciar el proceso
+      myApp.showPreloader('Registrando...');
+       
+      // Envía la solicitud al servidor , serializa a json los datos
+      var posting = $.post( url, { name : name , email:email , password:password ,
+      cedula:cedula , telefono:telefono , fecha_nacimiento:fecha_nacimiento , 
+      domicilio:domicilio } )
+     
+        //Si la respuesta del servidor fue satisfactoria
+       .done(function( data ) {
+
+        myApp.hidePreloader(); //Esconde el preloader
+
+        //Si el estado fue exitoso (Registro completo)
+        if (data.status == 'success') {
+            myApp.alert(data.message , 'Correcto');
+        }
+        
+        //Si el estado fue fallido (Email o telefono repetido)
+        if (data.status == 'fail') {
+            myApp.alert(data.message , 'Error');
+        }
+
+      })//end .done
+
+       //Si la solicitud al servidor fue erronea
+      .fail(function() {
+
+        myApp.hidePreloader(); //Esconde el preloader
+
+        //Muestra una alerta
+        myApp.alert('Ha ocurrido un error al contactar al servidor' , 'Sin conexión');
+      }); //End .fail
+      
+
+    }); //End evento on submit del formulario
+
+}) // End vista de registro
