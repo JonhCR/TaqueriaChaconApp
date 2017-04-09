a5fd902df4ae95e9c60821253cb24981f5fa8918
Jhon Moreira <jonh.m.10@gmail.com>
2017-04-08 19:08:06 -0600

    Login , register ,index , pedidos
    Logica robusta para el login incluida , prevencion del button back ,
    logica en las vistas de registro , login , index  incluida

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,6 +1,11 @@
+var storage = window.localStorage;
+
 // Inicializa la aplicacion
 var myApp = new Framework7({
             material : true, //Activa el tema de material css en framework7
+           // pushState: true, // El back button lo manejo manual , por eso comentado
+            modalButtonOk : 'Si' , // Ok button del modal
+            modalButtonCancel : 'Cancelar', // Cancel buttom del modal
         });
 
 // Si necesito usar Libreria personalizada del DOM se salva en $$
@@ -13,11 +18,167 @@ var mainView = myApp.addView('.view-main', {
 
 // Maneja el evento de cordova de dispositivo listo
 $$(document).on('deviceready', function() {
+   
+    var token = storage.getItem('token'); // Obtiene el token en el telefono
+
+    //Si el dispositivo no tiene un token de autenticacion
+    if(typeof token == null || !token || token == 'null' ){
+          mainView.router.loadPage("login.html");
+    }
+    else{
+      //Determina si el token aun sigue siendo valido en el servidor
+      if( !userAuthenticated() ){
+          mainView.router.loadPage("login.html");
+      }
+    }
+
     console.log("El dispositivo está listo");
 });
 
+myApp.onPageInit('index' , function(page){
 
-//Funcion anonima al cargarse la pantalla de registro
+    // Inicializa el swiper de imagenes dentro del index
+    var mySwiper = myApp.swiper('.swiper-container', {
+      pagination:'.swiper-pagination'
+    });
+
+    //Onclick de cerrar sesion
+    //Invalida el token en el servidor
+    //Destruye el token en storage del telefono
+    $('#logout_btn').click( function(){
+                myApp.confirm('¿Cerrar Sesion?', 'El chacolin colorado', 
+                  function () {
+                      myApp.showPreloader('Finalizando...');
+                      $.get( "http://demosolutionscrc.ga/api/cliente/logout?token="+storage.getItem('token'))
+                        .done(function(data) {
+                          myApp.hidePreloader(); // Esconde el preloader
+                          storage.setItem('token', null); // Elimina el token de autenticacion
+                          mainView.router.loadPage("login.html");
+                        })
+                        .fail(function() {
+                          myApp.hidePreloader(); // Esconde el preloader
+                          myApp.alert('Ha ocurrido un error , vuelve a intentarlo' , 'Error');
+                        });
+                  },
+                  function () {
+                       console.log("Cancelo cerrar sesion");
+                  }
+                );
+    });
+
+}).trigger();
+
+//Vista de autenticacion 
+myApp.onPageInit('login' , function(page){
+        
+        //Onclick al formulario de login
+        //Crea un token de autenticacion en el servidor
+        //Crea un token de autenticacion en el telefono
+        $('#logearClienteForm').submit(function( event ){
+
+            event.preventDefault();
+            
+            // Obtiene los datos del formulario
+            var $form = $( this ),
+            email = $form.find( "input[name='email']" ).val(),
+            password = $form.find( "input[name='password']" ).val(),
+            url = $form.attr( "action" );
+
+            //Muestra el preloader al inciar el proceso
+            myApp.showPreloader('Iniciando...');
+
+             // Envía la solicitud al servidor , serializa a json los datos
+              var posting = $.post( url, { email:email , password:password } )
+             
+                //Si la respuesta del servidor fue satisfactoria
+               .done(function( data ) {
+                storage.setItem('token', data.token) // Almacena el token de seguridad
+                myApp.hidePreloader(); //Esconde el preloader
+                mainView.router.loadPage("index.html");
+              })//end .done
+
+               //Si la solicitud al servidor fue erronea
+              .fail(function() {
+                myApp.alert('Tus credenciales son incorrectas' , 'Error');
+                myApp.hidePreloader(); //Esconde el preloader
+              }); //End .fail
+        });
+});
+
+//Step 1  de pedidos , utiliza una funciona anonima para
+//traerse los datos del cliente y el menu de la taqueria
+myApp.onPageInit('pedidos', function (page) {
+
+    //Carga la informacion del usuario
+    //Llena los selects del menú
+    (function(){ 
+        var user  = $.get( "http://demosolutionscrc.ga/api/cliente/pedidos?token="+storage.getItem('token'))
+                      .done(function(data) {
+                        console.log(data);
+                        
+                         var menus = data.menu;
+
+                         for ( i in menus) {       
+
+                            if (menus[i].categoria == 'Comidas') {
+
+                                myApp.smartSelectAddOption('#selector_comidas', 
+                                "<option"
+                                +" data-option-class=img-small lazy lazy-fadeIn"
+                                +" data-option-image=http://demosolutionscrc.ga"+ menus[i].foto
+                                +" value='apple'>"
+                                +menus[i].nombre
+                                +"</option>");
+
+                            }
+
+                            if (menus[i].categoria == 'Bebidas') {
+
+                                myApp.smartSelectAddOption('#selector_bebidas', 
+                                "<option"
+                                +" data-option-class=img-small lazy lazy-fadeIn"
+                                +" data-option-image=http://demosolutionscrc.ga"+ menus[i].foto
+                                +" value='apple'>"
+                                +menus[i].nombre
+                                +"</option>");
+
+                            }
+
+                            if (menus[i].categoria == 'Postres') {
+
+                                myApp.smartSelectAddOption('#selector_postres', 
+                                "<option"
+                                +" data-option-class=img-small lazy lazy-fadeIn"
+                                +" data-option-image=http://demosolutionscrc.ga"+ menus[i].foto
+                                +" value='apple'>"
+                                +menus[i].nombre
+                                +"</option>");
+
+                            }
+                             
+                           }
+                      })
+                      .fail(function() {
+                        myApp.alert('Error al cargar la informacion del usuario' , 'Error');
+                      });
+    })();
+
+    $('#pedidos-step1').click(function(){
+        mainView.router.loadPage("pedidos_step2.html");
+    });
+
+}) // End vista de pedidos
+
+//Pedidos step 2 obtiene informacion de envio 
+myApp.onPageInit('pedidos_step2', function (page) {
+  
+    $('#pedidos-step2').click(function(){
+        mainView.router.loadPage("pedidos_step3.html");
+    });
+
+}) // End vista de pedidos
+
+//Carga la vista para un nuevo registro
 myApp.onPageInit('register', function (page) {
 
     // Metodo submit del formulario 
@@ -53,6 +214,8 @@ myApp.onPageInit('register', function (page) {
         //Si el estado fue exitoso (Registro completo)
         if (data.status == 'success') {
             myApp.alert(data.message , 'Correcto');
+            //Lo traslada a login layout
+            mainView.router.loadPage("login.html");
         }
         
         //Si el estado fue fallido (Email o telefono repetido)
@@ -71,7 +234,30 @@ myApp.onPageInit('register', function (page) {
         myApp.alert('Ha ocurrido un error al contactar al servidor' , 'Sin conexión');
       }); //End .fail
       
-
     }); //End evento on submit del formulario
 
 }) // End vista de registro
+
+
+/**
+ * Valida si el usuario tiene una session activa
+ * @return boolean
+ */
+function userAuthenticated(){
+
+   var auth = false;
+
+       $.ajax({
+          url: "http://demosolutionscrc.ga/api/cliente/check/auth?token="+storage.getItem('token'),
+          async: false,
+        })
+         .done(function(data) {
+           console.log(data.session_msg);
+           auth = data.session_status;
+         })
+         .fail(function() {
+           auth = false;
+         });
+
+    return auth;
+}
